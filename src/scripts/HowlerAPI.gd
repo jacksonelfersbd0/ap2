06a4a1c2c0bb11d561b7697c7a11cb9cd8b45f8f
jacksonelfersbd0 <jacksonelfersbd0@outlook.com>
2024-12-22 13:01:43 -0800

    add: new test

--- src/scripts/HowlerAPI.gd
+++ src/scripts/HowlerAPI.gd
@@ -1,30 +1,48 @@
-# HowlerAPI.gd
 extends Node
 
-var howler_script = "let sound = null;"
+# Initialize the sound object globally in JavaScript only when needed
+var sound_initialized = false
 
 # Load the Howler.js audio file
 func load_sound(path: String, volume: float = 1.0) -> void:
-	var js_code = """
-		if (sound) sound.stop();  // Stop any previously playing sound
-		sound = new Howl({
-			src: ['""" + path + """'],
-			volume: """ + str(volume) + """
-		});
-	"""
-	JavaScript.eval(js_code)
+	if not sound_initialized:
+		# Initialize the sound object in JavaScript when first loading
+		var js_code = """
+			var sound = new Howl({
+				src: ['""" + path + """'],
+				volume: """ + str(volume) + """
+			});
+		"""
+		JavaScript.eval(js_code)
+		sound_initialized = true  # Now sound is initialized
+	else:
+		# Just change the source of the already initialized sound
+		var js_code = """
+			sound.stop();  // Stop any previously playing sound
+			sound = new Howl({
+				src: ['""" + path + """'],
+				volume: """ + str(volume) + """
+			});
+		"""
+		JavaScript.eval(js_code)
 
 # Play the sound
 func play_sound() -> void:
-	var js_code = "if (sound) sound.play();"
+	var js_code = """
+		if (typeof sound !== 'undefined') sound.play();
+	"""
 	JavaScript.eval(js_code)
 
 # Stop the sound
 func stop_sound() -> void:
-	var js_code = "if (sound) sound.stop();"
+	var js_code = """
+		if (typeof sound !== 'undefined') sound.stop();
+	"""
 	JavaScript.eval(js_code)
 
 # Set the volume
 func set_volume(volume: float) -> void:
-	var js_code = "if (sound) sound.volume(" + str(volume) + ");"
+	var js_code = """
+		if (typeof sound !== 'undefined') sound.volume(""" + str(volume) + """);
+	"""
 	JavaScript.eval(js_code)
